bdd9ad39c8f0a27588acdb9a99932f6f9524083e
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-03-07 21:00:59 -0600

    Time: 6 ms, Memory: 46.4 MB - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length-1; i+=2){
+            if(nums[i] != nums[i-1]){
+                return nums[i-1];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
