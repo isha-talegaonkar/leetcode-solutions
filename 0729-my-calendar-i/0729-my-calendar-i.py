b9cd8c057d5b09b4e83aec8dd5f3dc21de770b88
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-07-25 23:16:25 -0500

    Time: 151 ms (94.14%), Space: 17.5 MB (33.87%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,34 +1,35 @@
-class MyCalendar(object):
+class MyCalendar:
 
     def __init__(self):
-        self.calender = []
+        self.calendar = []
 
-    def book(self, start, end):
-        """
-        :type start: int
-        :type end: int
-        :rtype: bool
-        """
-        l = 0
-        r = len(self.calender)
-
-        while l < r:
-            m = (l + r) // 2
-
-            if self.calender[m][0] <= start:
-                l = m + 1
+    def book(self, start: int, end: int) -> bool:
+#         for s, e in self.calendar:
+#             if s < end and start < e:
+#                 return False
+            
+#         self.calendar.append((start, end))
+#         return True
+        left = 0
+        right = len(self.calendar)
+        
+        while left < right:
+            middle = left + (right - left) // 2
+            
+            if self.calendar[middle][0] <= start:
+                left = middle + 1
             else:
-                r = m
+                right = middle
         
-        if self.intersection(l, start, end):
+        if self.intersection(left, start, end):
             return False
-        else:
-            self.calender.insert(l, [start, end])
-        return True
-
-    def intersection(self, idx, s, e):
-        return (self.calender[idx-1][1] > s if idx >= 1 else False) or (self.calender[idx][0] < e if idx < len(self.calender) else False)
         
+        self.calendar.insert(left, [start, end])
+        return True
+        
+    
+    def intersection(self, idx, s, e):
+        return (self.calendar[idx-1][1] > s if idx >= 1 else False) or (self.calendar[idx][0] < e if idx < len(self.calendar) else False)
 
 
 # Your MyCalendar object will be instantiated and called as such:
