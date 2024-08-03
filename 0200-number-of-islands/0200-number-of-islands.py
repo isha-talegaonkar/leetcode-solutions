db365b71b92b4cc3643858232db35ceaed2af7f0
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-03 11:29:52 -0700

    Time: 249 ms (51.56%), Space: 18.8 MB (73.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,19 @@
 class Solution:
+    def isValid(self, grid, r, c):
+        if len(grid) > r >= 0 and len(grid[0]) > c >= 0:
+            return True
 
+    def dfs(self, grid, r, c):
+        if self.isValid(grid, r, c) and grid[r][c] == "1":
+            grid[r][c] = "0"
+        else:
+            return
+        
+        self.dfs(grid, r-1, c)
+        self.dfs(grid, r+1, c)
+        self.dfs(grid, r, c-1)
+        self.dfs(grid, r, c+1)
+        
     def numIslands(self, grid: List[List[str]]) -> int:
 
         if not grid:
@@ -16,20 +30,6 @@ class Solution:
                     numIslands += 1
         return numIslands
     
-    def isValid(self, grid, r, c):
-        if len(grid) > r >= 0 and len(grid[0]) > c >= 0:
-            return True
 
-    def dfs(self, grid, r, c):
-        if self.isValid(grid, r, c) and grid[r][c] == "1":
-            grid[r][c] = "0"
-        else:
-            return
-        
-        self.dfs(grid, r-1, c)
-        self.dfs(grid, r+1, c)
-        self.dfs(grid, r, c-1)
-        self.dfs(grid, r, c+1)
-        
         
             
