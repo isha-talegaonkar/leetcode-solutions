0b40fa2a27f0d62ead9310e7bb80554df052dff5
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-06 00:18:21 -0700

    Time: 161 ms (83.65%), Space: 17.3 MB (80.84%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -4,34 +4,26 @@ class MyCalendar:
         self.calendar = []
 
     def book(self, start: int, end: int) -> bool:
-#         for s, e in self.calendar:
-#             if s < end and start < e:
-#                 return False
-            
-#         self.calendar.append((start, end))
-#         return True
         left = 0
         right = len(self.calendar)
         
         while left < right:
-            middle = left + (right - left) // 2
+            mid = left + (right-left) // 2
             
-            if self.calendar[middle][0] <= start:
-                left = middle + 1
+            if self.calendar[mid][0] <= start:
+                left = mid + 1
             else:
-                right = middle
-        
-        if self.intersection(left, start, end):
+                right = mid
+            
+        if self.intersects(left, start, end):
             return False
         
         self.calendar.insert(left, [start, end])
         return True
+
+    def intersects(self, index, start, end):
+        return ((index >= 1 and self.calendar[index-1][1] > start) or (index < len(self.calendar) and self.calendar[index][0] < end))
         
-    
-    def intersection(self, idx, s, e):
-        return (self.calendar[idx-1][1] > s if idx >= 1 else False) or (self.calendar[idx][0] < e if idx < len(self.calendar) else False)
-
-
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
