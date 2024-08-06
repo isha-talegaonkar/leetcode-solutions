573a5bddf192e3df79f96c568b0ffea8845ea61c
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-06 09:45:48 -0700

    Time: 1229 ms (92.68%), Space: 79 MB (80.49%) - LeetHub

--- 2747-count-zero-request-servers/2747-count-zero-request-servers.py
+++ 2747-count-zero-request-servers/2747-count-zero-request-servers.py
@@ -2,27 +2,30 @@ from collections import defaultdict
 
 class Solution:
     def countServers(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
-        queries = [(i, q - x, q) for i, q in enumerate(queries)]
-
-        queries.sort(key = lambda x: x[1])
-        logs.sort(key=lambda x: x[1])
-        logs.append([0, float("inf")]) 
-        inactiveServers = [n] * len(queries)
-
-        left, right = 0, 0
+        queries = [(i, q-x, q) for i, q in enumerate(queries)]
+        queries.sort(key = lambda x:x[1])
+        
+        logs.sort(key = lambda x: x[1])
+        logs.append([0, float("inf")])
+        
         activeServers = defaultdict(int)
-
+        inactiveServers = [n] * len(queries)
+        
+        right, left = 0, 0
+        
         for i, start, end in queries:
             while logs[right][1] <= end:
                 activeServers[logs[right][0]] = activeServers[logs[right][0]] + 1
                 right += 1
+            
             while logs[left][1] < start:
                 activeServers[logs[left][0]] -= 1
-                if activeServers[logs[left][0]] == 0: 
+                if activeServers[logs[left][0]] == 0:
                     del activeServers[logs[left][0]]
-
                 left += 1
-
+            
             inactiveServers[i] -= len(activeServers)
-
+        
         return inactiveServers
+            
+            
