6eed4d20d6d77a5733543d733061070c16cd30ce
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-06 07:22:50 -0700

    Time: 54 ms (51.11%), Space: 16.6 MB (20.95%) - LeetHub

--- 1958-check-if-move-is-legal/1958-check-if-move-is-legal.py
+++ 1958-check-if-move-is-legal/1958-check-if-move-is-legal.py
@@ -3,27 +3,21 @@ class Solution:
         numRows = len(board)
         numCols = len(board[0])
         
-        directions = [[1,0], [0, 1], [-1, 0], [0, -1],
-                      [-1, 1], [1, -1], [1, 1], [-1, -1]
-                     ]
+        directions = [[0,1],[1,0],[0,-1], [-1,0],[-1,1],[1,-1],[-1,-1],[1,1]]
         
-        def isLegal(row, col, d, color):
-            row = row + d[0]
-            col = col + d[1]
+        def isLegal(row, col, d):
+            row, col = row + d[0], col + d[1]
             distance = 1
-            
-            while numRows > row >= 0 and numCols > col >= 0:
+            while row >= 0 and row < numRows and col >= 0 and col < numCols:
                 distance += 1
                 if board[row][col] == ".":
                     return False
-                if board[row][col] == color: 
+                if board[row][col] == color:
                     return distance >= 3
                 
                 row, col = row + d[0], col + d[1]
             return False
-
-        for d in directions:
-            if (isLegal(rMove, cMove, d, color)):
-                return True
         
-        return False
+        for d in directions:
+            if isLegal(rMove, cMove, d):
+                return True
