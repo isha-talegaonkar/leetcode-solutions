88eacde0faac69cb1e3523f11cea00c1de8c1f52
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-14 23:08:45 -0700

    Time: 108 ms (37.00%), Space: 17.7 MB (30.41%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(numbers)):
+            complement = target - numbers[i]
+            
+            if complement in hashmap:
+                return [hashmap[complement]+1, i+1]
+            hashmap[numbers[i]] = i
+                
+                
