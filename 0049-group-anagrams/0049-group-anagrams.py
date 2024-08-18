e28418138801431c790674fcd832fe7670000386
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-18 16:20:41 -0700

    Time: 104 ms (13.97%), Space: 22.3 MB (9.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
         
-        for word in strs:
-            hashmap[tuple(sorted(word))].append(word)
-        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            hashmap[tuple(count)].append(s)
         return hashmap.values()
