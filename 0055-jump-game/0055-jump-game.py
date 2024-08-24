cb6ad8982a42dee675268b948d3564020501ca59
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-08-24 13:05:24 -0700

    Time: 337 ms (94.24%), Space: 17.7 MB (67.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,24 +1,32 @@
 class Solution:
-    def __init__(self):
-        self.memo = []
-        self.nums = []
-        
-    def isJumpPossible(self, currentPosition) -> bool:
-        if self.memo[currentPosition] != -1:
-            return self.memo[currentPosition] == 1
-        
-        furthestJump = min(currentPosition + self.nums[currentPosition], len(self.nums) - 1)
-        
-        for nextPosition in range(currentPosition+1, furthestJump+1):
-            if self.isJumpPossible(nextPosition):
-                self.memo[currentPosition] = 1
-                return True
-        self.memo[currentPosition] = 0
-        return False
-        
-        
     def canJump(self, nums: List[int]) -> bool:
-        self.memo =[-1] * len(nums)
-        self.memo[-1] = 1
-        self.nums = nums
-        return self.isJumpPossible(0)
+        lastPos = len(nums) - 1
+        for i in range(len(nums) -1, -1, -1):
+            if(i+nums[i] >= lastPos):
+                lastPos = i
+        
+        return lastPos == 0
+    
+#     def __init__(self):
+#         self.memo = []
+#         self.nums = []
+        
+#     def isJumpPossible(self, currentPosition) -> bool:
+#         if self.memo[currentPosition] != -1:
+#             return self.memo[currentPosition] == 1
+        
+#         furthestJump = min(currentPosition + self.nums[currentPosition], len(self.nums) - 1)
+        
+#         for nextPosition in range(currentPosition+1, furthestJump+1):
+#             if self.isJumpPossible(nextPosition):
+#                 self.memo[currentPosition] = 1
+#                 return True
+#         self.memo[currentPosition] = 0
+#         return False
+        
+        
+#     def canJump(self, nums: List[int]) -> bool:
+#         self.memo =[-1] * len(nums)
+#         self.memo[-1] = 1
+#         self.nums = nums
+#         return self.isJumpPossible(0)
