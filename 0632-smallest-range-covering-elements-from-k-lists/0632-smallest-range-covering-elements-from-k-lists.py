47e47429f1e03d9a6b3d94bec2d72e2deb834ce8
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-09-05 20:31:55 -0700

    Time: 4087 ms (5.10%), Space: 22.9 MB (93.93%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -2,18 +2,18 @@ class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         unique_numbers = sorted(set(num for lst in nums for num in lst))
         max_value = min(lst[-1] for lst in nums)
-
+        
         unique_numbers = [num for num in unique_numbers if num <= max_value]
-        result_range = [0, math.inf]
-
+        result = [0, math.inf]
+        
         for left in unique_numbers:
-            min_values = []
+            min_numbers = []
             for lst in nums:
-                min_value = (min(num for num in lst if num >= left))
-                min_values.append(min_value)
-            right = max(min_values)
-
-            if right - left < result_range[1] - result_range[0]:
-                result_range = [left, right]
-
-        return result_range
+                min_value = min(num for num in lst if num >= left)
+                min_numbers.append(min_value)
+            right = max(min_numbers)
+            
+            if right - left < result[1] - result[0]:
+                result = left, right
+        
+        return result
