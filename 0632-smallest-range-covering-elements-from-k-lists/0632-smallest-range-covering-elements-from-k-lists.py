309c523dd27d2ef86cedc0d70e582aff690335d0
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-09-05 22:11:49 -0700

    Time: 208 ms (28.93%), Space: 22.9 MB (93.93%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -1,23 +1,26 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         heap = []
-      
         curr_max = float('-inf')
         for i in range(len(nums)):
-            heappush(heap,[nums[i][0], i, 0])
-            curr_max = max(curr_max,nums[i][0])
+            heappush(heap, [nums[i][0], i, 0])
+            curr_max = max(curr_max, nums[i][0])
         
-        best_max =  curr_max
+        best_max = curr_max
         best_min = heap[0][0]
+        
         while len(heap) == len(nums):
             val, i, j = heappop(heap)
             if j+1 >= len(nums[i]):
                 continue
-            next_val = nums[i][j+1]
-            heappush(heap, [next_val,i,j+1])
+            
+            heappush(heap, [nums[i][j+1], i, j+1])
             curr_min = heap[0][0]
-            curr_max = max(curr_max,next_val)
+            curr_max = max(curr_max, nums[i][j+1])
+            
             if curr_max - curr_min < best_max - best_min:
-                best_max = curr_max
-                best_min = curr_min
+                best_min, best_max = curr_min, curr_max
+            
+        
         return [best_min, best_max]
+            
