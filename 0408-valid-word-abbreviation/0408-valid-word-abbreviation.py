816f366ba150c4cb70451a7ddc89fae35d954b0f
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-11-17 20:02:21 -0800

    Time: 0 ms (100.00%), Space: 16.5 MB (71.12%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,7 +1,9 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        wordLen = len(word)
+        abbrLen = len(abbr)
+        
         wordPointer, abbrPointer = 0, 0
-        wordLen, abbrLen = len(word), len(abbr)
         
         while wordPointer < wordLen and abbrPointer < abbrLen:
             if abbr[abbrPointer].isdigit():
@@ -9,16 +11,15 @@ class Solution:
                     return False
                 skip = 0
                 while abbrPointer < abbrLen and abbr[abbrPointer].isdigit():
-                    skip = skip * 10 + int(abbr[abbrPointer])
+                    skip = 10 * skip + int(abbr[abbrPointer])
                     abbrPointer += 1
                 wordPointer += skip
-                if wordPointer > wordLen:
+                if wordPointer >wordLen:
                     return False
             else:
                 if word[wordPointer] != abbr[abbrPointer] or wordPointer >= wordLen:
                     return False
                 wordPointer += 1
                 abbrPointer += 1
-                
         return wordPointer == wordLen and abbrPointer == abbrLen
         
