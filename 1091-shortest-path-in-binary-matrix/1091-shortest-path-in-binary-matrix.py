33447ab27865c88e6cd8ef8e222957bea6d1542c
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2024-11-24 22:39:07 -0800

    Time: 263 ms (24.40%), Space: 18.4 MB (18.36%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,38 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        maxRow = len(grid) - 1
+        maxCol = len(grid[0]) - 1
+        
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        
+        if grid[0][0] != 0 or grid[maxRow][maxCol] != 0:
+            return -1
+        
+        def getNeighbors(row, col):
+            for rowDiff, colDiff in directions:
+                newRow = row + rowDiff
+                newCol = col + colDiff
+                if not (0 <= newRow <= maxRow and 0 <= newCol <= maxCol):
+                    continue
+                if grid[newRow][newCol] != 0:
+                    continue
+
+                yield (newRow, newCol)
+        
+        queue = deque()
+        queue.append((0,0,1))
+        visited = {(0,0)}
+        
+        while queue:
+            row, col,distance = queue.popleft()
+            if (row, col) == (maxRow, maxCol):
+                return distance
+            for neighborR, neighborC, in getNeighbors(row, col):
+                if (neighborR, neighborC) in visited:
+                    continue
+                visited.add((neighborR, neighborC))
+                queue.append((neighborR, neighborC, distance+1))
+            
+        return -1
+                
